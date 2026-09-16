418a2d72ffdafe88d3c94cd1214f1adb9514b5ee
Andrei Moskvin <andrey.mskv@gmail.com>
2026-09-16 16:01:51 +0400

    add YaCutForm & YaCutAddFilesForm

---
+++ yacut/forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import URLField, SubmitField, StringField
+from wtforms.validators import DataRequired, Length, Optional, URL
+from flask_wtf.file import MultipleFileField, FileSize, FileRequired
+
+
+class YaCutForm(FlaskForm):
+    original_link = URLField(
+        'Длинная ссылка',
+        validators=[
+            DataRequired(message='Обязательное поле'),
+            URL(message='Введите корректную ссылку'),
+            Length(1, 2048),
+        ],
+    )
+    custom_id = StringField(
+        'Ваш вариант короткой ссылки',
+        validators=[
+            Length(
+                max=16,
+                message='Не более 16 символов.',
+            ),
+            Optional(),
+        ],
+    )
+    submit = SubmitField('Создать')
+
+
+class YaCutAddFilesForm(FlaskForm):
+    images = MultipleFileField(
+        validators=[
+            FileRequired(message='Добавьте хотя бы один файл.'),
+            FileSize(
+                max_size=10 * 1024 * 1024,
+                message='Размер файла не должен превышать 10 МБ.',
+            ),
+        ],
+    )
+    submit = SubmitField('Загрузить')
