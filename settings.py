f1edd371a4a6e29c0b86905ffc4d0459006c9297
Andrei Moskvin <andrey.mskv@gmail.com>
2026-09-16 16:11:01 +0400

    add max content lenght in settings

--- settings.py
+++ settings.py
@@ -9,3 +9,6 @@ class Config(object):
         'DATABASE_URI'
     )  # Строка подключения к базе данных.
     YANDEX_TOKEN = os.getenv('DISK_TOKEN')  # Токен для работы с Яндекс.Диском.
+    MAX_CONTENT_LENGTH = int(
+        os.getenv('MAX_CONTENT_LENGTH', 100 * 1024 * 1024)
+    )  # Максимальный размер загружаемых файлов.
